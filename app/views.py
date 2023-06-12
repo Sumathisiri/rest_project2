83017994f38ba83e1f7938fe0a156a1488e9e912
sumathisiri <mekalasiri16@gmail.com>
2023-06-12 21:58:26 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -8,13 +8,42 @@ from app.serializers import *
 from rest_framework.response import Response
 
 
+
 class ProductCrud(APIView):
-    def get(self,request):
+    def get(self,request,id):
         PQS=Product.objects.all()
         PJD=ProductSerializer(PQS,many=True)
         return Response(PJD.data)
 
-    def post(self,request):
-        pass
+    def post(self,request,id):
+        PMSD=ProductSerializer(data=request.data)
+        if PMSD.is_valid():
+            PMSD.save()
+            return Response({'message':'Product is created'},)
+        else:
+            return Response({'failed':'Product is not created'})
+    
+
+    def put(self,request,id):
+        id=request.data['id']
+        PO=Product.objects.get(id=id)
+        UPO=ProductSerializer(PO,data=request.data)
+        if UPO.is_valid():
+            UPO.save()
+            return Response({'message':'Product is updated'})
+        return Response({'failed':'Product is not updated'})
+
+    def patch(self,request,id):
+        id=request.data['id']
+        PO=Product.objects.get(id=id)
+        PO=Pname=request.data['Pname']
+        PO.save()
+        return Response({'success':'Data is partially Updated'})
+
+
+    def delete(self,request,id):
+        Product.objects.get(id=id).delete()
+        return Response({'success':'Product is Deleted'})
+
 
 
--- rest_project2/urls.py
+++ rest_project2/urls.py
@@ -20,5 +20,6 @@ from app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ProductCrud/',ProductCrud.as_view(),name='ProductCrud'),
+    path('ProductCrud/<id>',ProductCrud.as_view(),name='ProductCrud'),
+
 ]
